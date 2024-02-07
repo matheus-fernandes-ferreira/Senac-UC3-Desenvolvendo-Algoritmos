c114b724f86599f3f4ad8883d847eb5d43405393
lucasilvafe <luksilvafe@gmail.com>
2024-02-07 15:47:46 -0300

    Jogo genis

--- projetoGenius.js
+++ projetoGenius.js
@@ -1,37 +1,56 @@
-
-function iniciarJogo()
-{
-    var pontuacao = 0;
-    var nivel = 1;
-    var vetor = [[1,2,3,4]];
-    var sequenciaMaquina = [];
-    var sequenciaDoJogador = []; 
+var nivel = 1;
+var pontuacao = 0;
+var nivel = 1;
+var vetor = [[1, 2, 3, 4]];
+var sequenciaMaquina = [];
+var sequenciaDoJogador = [];
+function iniciarJogo() {
+    
 
 }
-while(true)
-{
-    function rodada()
-    {
-
+while (true) {
+    rodada()
+    if (resultado = false) {
+        fimDeJogo()
+    } else {
+        pontuacao += 1;
+        nivel += 1;
     }
-    if ()
-    {
-        function fimDejogo()
-        {
-            return false
-        }
-    }
-    pontuacao+=10
+    fimDejogo()
+    pontuacao += 10
     nivel++
 }
-function rodada()
-{
-    sequenciaMaquina = gerarsequencia(nivel)
-    
+function rodada() {
+    sequenciaMaquina = gerarsequencia()
+    exibirSequencia(sequencia)
+    var jogadorSequencia = lerJogada()
+    var resultado = verificarJogada(jogadorSequencia, sequencia)
+    if (resultado == false) {
+        return false
+    }
 }
 
+function gerarsequencia() {
 
+    var sequenciaMaquina = [];
+    for (let i = 0; i < nivel; i++) {
+        var numero = gerarNumeroAleatorio()
+        sequenciaMaquina.push(numero)
 
+        return sequenciaMaquina
+
+    }
+
+    function gerarNumeroAleatorio() {
+        let numeroAleatorio = vetor[Math.floor(Math.random() * vetor.length)];
+        return numeroAleatorio
+    }
+
+}
+
+function fimDeJogo() {
+    console.log("fim de jogo");
+}
 
 
 
