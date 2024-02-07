f684a5be01288b2afa66a70e0f9439a12ada2aa6
lucasilvafe <luksilvafe@gmail.com>
2024-02-07 15:00:32 -0300

    Iniciando o jogo

--- projetoGenius.js
+++ projetoGenius.js
@@ -1,1 +1,53 @@
 
+function iniciarJogo()
+{
+    var pontuacao = 0;
+    var nivel = 1;
+    var vetor = [[1,2,3,4]];
+    var sequenciaMaquina = [];
+    var sequenciaDoJogador = []; 
+
+}
+while(true)
+{
+    function rodada()
+    {
+
+    }
+    if ()
+    {
+        function fimDejogo()
+        {
+            return false
+        }
+    }
+    pontuacao+=10
+    nivel++
+}
+function rodada()
+{
+    sequenciaMaquina = gerarsequencia(nivel)
+    
+}
+
+
+
+
+
+
+
+
+
+/*while(true)
+{
+    for(i = 0; i < 10; i++)
+    {
+        console.log(i)
+        if(i >= 4)
+    {
+        return false
+    }
+    }
+    
+}
+*/
